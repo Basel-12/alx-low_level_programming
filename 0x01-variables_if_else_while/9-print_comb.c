d9c033e9a8b1e6e3c6d2b8426123832fbbfd6cc6
Basel-12 <bassilhamdi410@gmail.com>
2023-07-14 16:00:16 +0300

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progrmam uses puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+{
+	putchar(48 + i);
+	if (i == 9)
+{
+		putchar('$');
+		break;
+}
+	putchar(',');
+	putchar(' ');
+}
+	return (0);
+}
+
--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -16,9 +16,12 @@ int main(void)
     for (i = 0; i <=9; i++)
     {
         putchar(48 + i);
-        if (i==9)
-            putchar('\n');
+        if (i==9){
+            putchar('$');
+            break;
+        }
+        putchar(',');
+        putchar(' ');
     }
-    
 	return (0);
 }
