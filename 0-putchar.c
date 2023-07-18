a5ebdc147e96d281f4a8ba096d7065d0dfbdcfc1
Basel-12 <bassilhamdi410@gmail.com>
2023-07-18 14:05:39 +0300

    0x02. C - Functions, nested loops

---
+++ 0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progrmam uses puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
