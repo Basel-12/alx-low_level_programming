e558fd660849eec24099a7d33770552ae58b2418
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 16:38:35 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * add - function to get last digit of a number
+ *
+ * @n: checks input of function
+ *
+ * @a: input of function
+ *
+ * Return: returns 1 if 'c' is alphabet otherwise returns 0
+ *
+*/
+int add(int a, int n)
+{
+	return (a + n);
+}
