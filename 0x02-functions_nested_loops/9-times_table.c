beb9d420e0a1c2930332eb552d710b4711329cef
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 16:35:09 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,8 @@
 #include <time.h>
 #include<stdio.h>
 #include<unistd.h>
+#include "main.h"
+
 
 /**
  * times_table - print the time table
@@ -12,23 +14,21 @@ void times_table(void)
 
 	for (hh = 0; hh <= 9; hh++)
 {
+	_putchar(48);
 	for (mm = 1; mm <= 9; mm++)
 {
+		_putchar(',');
+		_putchar(' ');
 		res = hh * mm;
 		if (res <= 9)
 {
-			putchar(' ');
+			_putchar(' ');
 }
-		putchar(res / 10 + '0');
-	if (res > 9)
-		putchar(res % 10 + '0');
-	if (mm != 9)
-{
-		putchar(',');
-		putchar(' ');
+		else
+			_putchar(res / 10 + '0');
+
+		_putchar(res % 10 + '0');
 }
-}
-	if (hh != 9)
-		putchar('\n');
+		_putchar('\n');
 }
 }
