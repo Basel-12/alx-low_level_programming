13249e200223b27717ee38fdd78c360be439de1d
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 14:00:37 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_last_digit - function to get last digit of a number
+ *
+ * @n: checks input of function
+ *
+ * Return: returns 1 if 'c' is alphabet otherwise returns 0
+ *
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
