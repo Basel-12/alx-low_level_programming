039bfe35c98c2c0a2f0266fcae5d169967b157be
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 12:04:54 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_sign - function to check if sign of a number
+ *
+ * @n: checks input of function
+ *
+ * Return: returns 1 if 'c' is alphabet otherwise returns 0
+ *
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_printcar('+');
+	return (1);
+
+}
+	if (n < 0)
+{
+	_printcar('-');
+	return (-1);
+}
+	_printcar('0');
+	return (0);
+}
