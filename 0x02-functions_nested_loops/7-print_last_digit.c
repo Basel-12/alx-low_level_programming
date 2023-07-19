456b5917b386ad2347e8d4b9ddfef0bc581c37bd
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 14:10:21 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,7 +15,12 @@
 */
 int print_last_digit(int n)
 {
+	int last;
+
 	if (n < 0)
-		return (-1 * (n % 10));
-	return (n % 10);
+		last = (-1 * (n % 10));
+	else
+		last = n % 10;
+	_putchar(last + '0');
+	return (last);
 }
