1af845014c5b51208ef9962efede4026f3103b8b
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 11:32:34 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - uses _putchar to print alphabet 10 times
+ *
+*/
+void print_alphabet(void)
+{
+	char i;
+	int k;
+
+	for (k = 1; k <= 10; ++k)
+{
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+}
