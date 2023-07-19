a2dc40cd6dc365edd8a7d5da91fcef428e7c5933
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 14:39:00 +0300

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * jack_bauer - function to get last digit of a number
+ *
+ * Return: returns 1 if 'c' is alphabet otherwise returns 0
+ *
+*/
+void jack_bauer(void)
+{
+	int hh, mm;
+
+	for (hh = 0; hh < 24; hh++)
+{
+	for (mm = 0; mm < 60; mm++)
+		_putchar((hh / 10) + '0');
+		_putchar((hh % 10) + '0');
+		_putchar(':');
+		_putchar((mm / 10) + '0');
+		_putchar((mm % 10) + '0');
+}
+}
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_sign - function to check if sign of a number
+ *
+ * @n: checks input of function
+ *
+ * Return: returns 1 if 'c' is alphabet otherwise returns 0
+ *
+*/
+int main(){
+    printf("%02d\n",4);
+}
