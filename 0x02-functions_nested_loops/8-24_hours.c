ba68efd1c08cadc37e12d36d40f0ee9c6c7e0eb1
Basel-12 <bassilhamdi410@gmail.com>
2023-07-19 14:41:21 +0300

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,10 +18,12 @@ void jack_bauer(void)
 	for (hh = 0; hh < 24; hh++)
 {
 	for (mm = 0; mm < 60; mm++)
+{
 		_putchar((hh / 10) + '0');
 		_putchar((hh % 10) + '0');
 		_putchar(':');
 		_putchar((mm / 10) + '0');
 		_putchar((mm % 10) + '0');
+		_putchar('\n');
 }
 }
