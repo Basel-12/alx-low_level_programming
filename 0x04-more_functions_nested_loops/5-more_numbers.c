e8713dba2acdefff041cc41870883c22edf85c6b
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 14:07:46 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * more_numbers - function to check if character lowercase
+ *
+ * Return: void
+ *
+*/
+void more_numbers(void)
+{
+	int c, k;
+
+	for (c = 1; c <= 10; c++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			_putchar(k % 10 + '0');
+			if (k > 9)
+				_putchar(k / 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
