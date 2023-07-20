98c4f9a98c6eef6a94e0a7d1de8671636b6b2db7
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 13:48:34 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_numbers - function to check if character lowercase
+ *
+ * Return: void
+ *
+*/
+void print_numbers(void)
+{
+	for (char c = 48; c <= 57; c++)
+		_putchar(c);
+	_putchar('\n');
+}
