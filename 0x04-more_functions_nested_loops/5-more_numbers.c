48d443b6c80a2ff21f97202717b5091f4169417b
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 14:31:33 +0300

    my commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,9 +19,9 @@ void more_numbers(void)
 	{
 		for (k = 0; k <= 14; k++)
 		{
-			_putchar(k % 10 + '0');
 			if (k > 9)
 				_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
 		}
 	_putchar('\n');
 	}
