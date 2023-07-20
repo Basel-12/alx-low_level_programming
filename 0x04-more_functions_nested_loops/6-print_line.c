50fdb79634810a6874d22963d58b5c907e0bc8db
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 14:36:07 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_line - function to check if character lowercase
+ *
+ * @n: function input
+ *
+ * Return: void
+ *
+*/
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c <= n; c++)
+		_putchar('-');
+
+	_putchar('\n');
+}
