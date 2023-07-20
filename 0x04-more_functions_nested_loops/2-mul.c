13e204fe3174522a5d74a45fc8a7b60e81ab449b
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 13:44:09 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * mul - function to check if character lowercase
+ *
+ * @a: checks input of function
+ *
+ * @b:input of the function
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
