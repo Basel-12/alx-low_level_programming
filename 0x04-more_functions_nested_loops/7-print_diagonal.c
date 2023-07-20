693370233cde31f1900348a94ee99f9bbefe0c32
Basel-12 <bassilhamdi410@gmail.com>
2023-07-20 16:54:32 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * print_diagonal - function to check if character lowercase
+ *
+ * @n: function input
+ *
+ * Return: void
+ *
+*/
+void print_diagonal(int n)
+{
+	int c, k;
+
+	for (c = 1; c <= n; c++)
+	{
+		for (k = 0; k <= c - 1; k++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
