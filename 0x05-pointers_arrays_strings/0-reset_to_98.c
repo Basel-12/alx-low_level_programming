cf56c879d13676f992484611d7efcfdd7f0e1d54
Basel hamdi <cursed@kali>
2023-07-24 13:55:42 +0300

    My commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * reset_to_98  - function
+ *
+ * @n: pointer of an integer
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
