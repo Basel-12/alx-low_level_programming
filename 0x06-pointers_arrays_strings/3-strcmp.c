971b7e391fc4a3f5731246c4293fc50e2dbb3f7c
Basel-12 <bassilhamdi410@gmail.com>
2023-07-26 12:34:36 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * _strcmp - function
+ *
+ * @s1: pointer of an integer
+ *
+ * @s2: pointer
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+int _strcmp(char *s1, char *s2)
+{
+while (*s1)
+{
+if (*s1 != *s2)
+	break;
+	s1++;
+	s2++;
+}
+return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
