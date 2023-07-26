b77b39d8669bdfc8a6615b42bcc6bf1d3bd4d4fd
Basel-12 <bassilhamdi410@gmail.com>
2023-07-26 12:45:38 -0700

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,12 +17,16 @@
 */
 int _strcmp(char *s1, char *s2)
 {
+int i = 0;
+
 while (*s1)
 {
 if (*s1 != *s2)
+{	i = (int) *s1 - (int) *s2;
 	break;
+}
 	s1++;
 	s2++;
 }
-return ((int) *s1 - (int) *s2);
+return (i);
 }
