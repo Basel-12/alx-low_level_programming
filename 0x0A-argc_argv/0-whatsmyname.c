c0a2be2df0fb42b20abdcfcc9d6f3821564731e3
Basel-12 <bassilhamdi410@gmail.com>
2023-08-04 11:35:09 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progrmam uses puts function
+ *
+ * @argc: input
+ *
+ * @argv: input
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
