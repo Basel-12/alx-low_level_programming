599aa2b5cb442c1c2314ea1ee39962a43aacffd7
Basel-12 <bassilhamdi410@gmail.com>
2023-08-04 09:18:09 -0700

    commit

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,12 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
 #include "main.h"
 
-/*
- *@strpbrk - function
+
+/**
+ * _strpbrk - function
  *
- *@s: input
+ * @s: pointer of an integer
  *
- *@accept: input
+ * @accept: pointer
  *
- *Return: pointer
- */ 
-char *_strpbrk(char *s, char *accept);
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
