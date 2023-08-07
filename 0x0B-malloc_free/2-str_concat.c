79c60bba7ecf2d30ec4b3ab1abcc353e62b0672f
Basel-12 <bassilhamdi410@gmail.com>
2023-08-07 03:01:37 -0700

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,15 +21,15 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		*s1 = '\0';
 	if (s2 == NULL)
-		s2 = '\0';
+		*s2 = '\0';
 	for (i = 0; s1[i] != '\0'; ++i)
 		++size1;
 	for (i = 0; s2[i] != '\0'; ++i)
 		++size2;
 	m = (char *)malloc(sizeof(char) * (size1 + size2)  + 1);
-	for (i = 0; i < size1 + size2; i++)
+	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
 			m[i] = s1[i];
