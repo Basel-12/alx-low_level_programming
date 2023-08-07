1dc24606d1fac31e20c5a191a1e0df0e720ccfe2
Basel-12 <bassilhamdi410@gmail.com>
2023-08-07 02:33:35 -0700

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * _strdup - function
+ *
+ * @str: size of an array
+ *
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	int i, size = 0;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		++size;
+	char *m = (char *)malloc(sizeof(char) * size + 1);
+
+	if (m == 0)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; ++i)
+		m[i] = str[i];
+	return (m);
+}
