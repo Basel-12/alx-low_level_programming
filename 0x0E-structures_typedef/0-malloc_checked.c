02fd15ef6fda76efade341aec3411a3d666e96f6
Basel-12 <bassilhamdi410@gmail.com>
2023-08-14 13:25:05 -0700

    commit

---
+++ 0x0E-structures_typedef/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * malloc_checked - function
+ *
+ * @b: size of an array
+ *
+ *
+ * Return: returns 1 if 'c' is lowercase otherwise returns 0
+ *
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *s =  malloc(b);
+
+	if (s == 0)
+		exit(98);
+	return (s);
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progrmam uses puts function
+ *
+ * Return: Always 0 (Success)
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
