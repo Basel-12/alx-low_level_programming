feddf17613ba41e260f68f72a39a1391d8363bca
Basel-12 <bassilhamdi410@gmail.com>
2023-08-15 00:44:34 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,8 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == NULL)
-		{
-			d->name = "(nil)";
-		}
-		printf("Name: %s\nAge: %.6f\nOwner: %s", d->name, d->age, d->owner);
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %S", d->owner ? d->owner : "(nil)");
 	}
 }
