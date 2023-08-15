61665c19781112dbdc84f5dcec50ecfb9d5a6b11
Basel-12 <bassilhamdi410@gmail.com>
2023-08-15 01:31:32 -0700

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,7 +4,26 @@
 #include<unistd.h>
 #include "dog.h"
 
+/**
+ * _strcpy - copy string to string
+ *
+ *@s: pointer to the output string
+ *
+ *@c: pointer to the string we need to copy
+ *
+ * Return: pointer
+*/
+char *_strcpy(char *s, char *c)
+{
+	int i = 0;
 
+	for (i = 0; c[i]; i++)
+	{
+		s[i] = c[i];
+	}
+	s[i] = '\0';
+	return (s);
+}
 /**
  * new_dog - function
  *
@@ -24,8 +43,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+	d->name = (char *)malloc(sizeof(char) * strlen(name) + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = (char *)malloc(sizeof(char) * strlen(owner) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	d->name = _strcpy(d->name, name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = _strcpy(d->owner, owner);
 	return (d);
 }
