b14e48a49dd16dd11a80236c5727fcc1a466ab50
Basel-12 <bassilhamdi410@gmail.com>
2023-08-16 02:20:21 -0700

    commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+#include<unistd.h>
+#include "function_pointers.h"
+
+
+/**
+ * array_iterator - function
+ *
+ * @array: array of int
+ *
+ * @size: size of array
+ *
+ * @action: function pointer
+ *
+ * Return: void
+ *
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && size && action)
+	{
+		int i = 0;
+
+		while (i < size)
+			action(array[i++]);
+	}
+}
