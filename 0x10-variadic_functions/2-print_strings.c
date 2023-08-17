5bd626069e4c04bda1523960c806f2214418cd6d
Basel-12 <bassilhamdi410@gmail.com>
2023-08-17 03:57:05 -0700

    commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+
+/**
+* print_strings - variadic function
+*
+* @separator: separator between paremtars
+*
+* @n: number of input
+*
+* Return: void
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", (str = va_arg(args, char *)) ? str : "(nill)");
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
