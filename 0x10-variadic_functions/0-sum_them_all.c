1816a9b232f53331ea3700292c9d0f0cbacd0e1a
Basel-12 <bassilhamdi410@gmail.com>
2023-08-17 03:38:31 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
-	unsigned  int i;
-	int sum = 0;
+	 int i = n, sum = 0;
 
 	if (!n)
 		return (0);
 	va_start(args, n);
-	for (i = 0; i < n; i++)
+	while (i--)
 		sum += va_arg(args, int);
 	va_end(args);
 	return (sum);
