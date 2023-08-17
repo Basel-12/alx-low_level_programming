f364205f99248c7a52bd6c0fd714411c67c42c54
Basel-12 <bassilhamdi410@gmail.com>
2023-08-17 02:10:12 -0700

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - variadic function
+ *
+ *
+ *@n: number of parameters
+ *
+ *
+ *Return: sum of numbers
+ *
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned  int i;
+	int sum = 0;
+	
+	va_start(args, n);
+	if (!n)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifndef VARIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTION_H*/
