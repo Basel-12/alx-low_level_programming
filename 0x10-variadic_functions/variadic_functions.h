dc294eb9147e3019b75e7dd09f75ab510edd8e29
Basel-12 <bassilhamdi410@gmail.com>
2023-08-18 00:55:22 -0700

    commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,8 +5,25 @@
 
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+
+/**
+ * token - sturct
+ *@t: char token
+ *
+ *@f: function pointer
+ *
+*/
+
+
+typedef struct token
+{
+  char *t;
+  void (*f)(char *, va_list);  
+}token_t;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
+void print_all(const char * const format, ...);
 #endif /* VARIADIC_FUNCTION_H*/
