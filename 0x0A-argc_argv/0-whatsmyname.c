5a36c73e9228b0b5333021d53ae713268b531fbf
Basel-12 <bassilhamdi410@gmail.com>
2023-10-02 09:38:25 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main  -  function print string
+*
+* @argc: size of array
+*
+*@argv: array of char pointers
+* return: void
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n",argv[0]);
+	return 0;
+}
