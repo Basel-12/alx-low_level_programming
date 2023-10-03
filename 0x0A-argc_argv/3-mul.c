d3e30d4192243bc68ec2fc218f94656029b6c972
Basel-12 <bassilhamdi410@gmail.com>
2023-10-03 08:31:18 -0700

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,6 +15,6 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	else
-		return (1, printf("Error\n"));
+		return (printf("Error\n"), 1);
 	return (0);
 }
