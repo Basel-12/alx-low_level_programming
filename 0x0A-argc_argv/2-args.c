ed9ed1a8f5ad47013e865f599feca0b1c03945af
Basel-12 <bassilhamdi410@gmail.com>
2023-10-03 08:07:17 -0700

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main  -  function print string
+*
+* @argc: size of array
+*
+*@argv: array of char pointers
+*
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
