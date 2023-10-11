dc02734396757055c29a11680ceab0f4da0d655f
Basel-12 <bassilhamdi410@gmail.com>
2023-10-11 05:47:22 -0700

    commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *new_dog - function that intialize struct element with given parameters
+ *
+ *
+ * @name: pointer to dog name
+ *
+ * @age: dog's age
+ *
+ * @owner: owner name
+ *
+ * Return: void
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog_t *d = (dog_t *)malloc(sizeof(dog_t));
+
+	if (!d)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	return (d);
+}
