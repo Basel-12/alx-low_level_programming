6901af0eb08ceba91755a351452dfa4a7dc529d9
Basel-12 <bassilhamdi410@gmail.com>
2023-10-11 05:18:21 -0700

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *init_dog - function that intialize struct element with given parameters
+ *
+ * @d: pointer to struct
+ *
+ * @name: pointer to dog name
+ *
+ * @age: dog's age
+ *
+ * @owner: owner name
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
