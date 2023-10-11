c3a66befb5e9ac1f54bdfa0685a8142627226e21
Basel-12 <bassilhamdi410@gmail.com>
2023-10-11 06:14:27 -0700

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,14 +16,36 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len1 = 0, len2 = 0, i = 0;
 
 	if (!name || age < 0 || !owner)
 		return (NULL);
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
-	d->name = name;
+	for (len1 = 0; name[len1]; len1++)
+		;
+	d->name = (dog_t *)malloc(sizeof(char) * len1 + 1);
+	if (!d->name)
+	{
+		free(d);
+		return (NULL);
+	}
+	for (len2 = 0; owner[len2]; len2++)
+		;
+	d->owner = (dog_t *) malloc(sizeof(char) * len2 + 1);
+	if (!d->owner)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	d->age = age;
-	d->owner = owner;
+	for (i = 0; name[i]; i++)
+		d->name[i] = name[i];
+	d->name[i] = '\0';
+	for (i = 0; owner[i]; i++)
+		d->owner[i] = owner[i];
+	d->owner[i] = '\0';
 	return (d);
 }
