fa7334616def0bb58fe10ac47adfeb22b3ec9e96
Basel-12 <bassilhamdi410@gmail.com>
2024-05-22 16:32:49 +0300

    alx push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+/**
+ * main - function
+ *
+ * Description: print using printf
+ *
+ * Return: 0
+ *
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
