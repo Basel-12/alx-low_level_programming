0541027e11e0934b90e95177a5653f62a5ee836c
Basel-12 <bassilhamdi410@gmail.com>
2024-05-22 16:29:41 +0300

    alx push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/**
+ * main - run program
+ *
+ * Desription: A c program print speific string
+ *
+ * Return: 0
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
