6a9dd03d3cb8da47b9a4aaae65a0fb2b1ed14872
Basel-12 <bassilhamdi410@gmail.com>
2024-05-23 14:31:40 +0300

    alx push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10; i++;)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 26; j++;)
+		for (j = 0; j < 26; j++)
 		{
 			_putchar('a' + j);
 		}
