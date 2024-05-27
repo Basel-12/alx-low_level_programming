aae4e9d63fbbe35a5fd70201ee529169af5553b9
Basel-12 <bassilhamdi410@gmail.com>
2024-05-27 20:54:46 +0300

    alx push

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - run program
+ *
+ * @argc: number of arguments
+ *
+ * @argv: array of pointers
+ *
+ * Description: print change
+ *
+ * Return: 0 or 1
+ */
 
-int main(int argc , char* argv[])
+
+int main(int argc, char *argv[])
 {
 	int x;
 
-
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return 1;
-        }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	x = atoi(argv[1]);
-        if(x < 0)
-                printf("0\n");
-        
-        else
-                printf("%d\n", ((x / 25) + (x % 25) / 10 + (x % 25 % 10) / 5 + (x % 25 % 10 % 5) / 2 + (x % 25 % 10 % 5 % 2) / 1));
-        return 0;
-
+	if (x < 0)
+		printf("0\n");
+	else
+		printf("%d\n", ((x / 25) + (x % 25) / 10 + (x % 25 % 10) / 5 + (x % 25 % 10 % 5) / 2 + (x % 25 % 10 % 5 % 2) / 1));
+	return (0);
 }
