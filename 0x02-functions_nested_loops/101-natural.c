dd88bf6d048bde190fba880f8b285fdac19bb729
Basel-12 <bassilhamdi410@gmail.com>
2024-05-27 15:17:55 +0300

    alx push

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - test program
+ *
+ * Description: sum naturan numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 5; i < 1024; i++;)
+	{
+		if (i % 3 == 0)
+			sum += i;
+		else if (i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
