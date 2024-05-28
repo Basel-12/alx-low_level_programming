a6c6243530543a81c5f25dc536085e99f1c818bb
Basel-12 <bassilhamdi410@gmail.com>
2024-05-28 09:36:01 +0300

    alx push

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum;
+        int i;
+        int value;
+        va_list args;
+
 	if (n == 0)
 		return (0);
-	int sum = 0, i, value;
-	va_list args;
-
 	va_start(args, n);
-
-	for (i = 0; i < n; i++)
+	sum = 0;
+	for (i = 0; i < (int)n; i++)
 	{
 		value = va_arg(args, int);
 		sum += value;
