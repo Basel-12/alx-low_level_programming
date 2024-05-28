396fe383b50b4009a881c7eea6fd5ccf6d535cb6
Basel-12 <bassilhamdi410@gmail.com>
2024-05-28 11:18:45 +0300

    alx push

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	int i;
-	char *value;
+	char *str;
 	va_list args;
 
 	if (!n)
@@ -28,8 +28,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < (int)n; i++)
 	{
-		value = va_arg(args, char *);
-		printf("%s%s", value ? value : "(nil)", i != (int)n - 1 ? separator ? separator : "" : "\n");
+		str = va_arg(args, char *) ? va_arg(args, char *) : "(nil)";
+		printf("%s%s", str, i != (int)n - 1 ? separator ? separator : "" : "\n");
 	}
 	va_end(args);
 }
