50f2849b0e198e33cb59c6da9a60d30cee6646d0
Basel-12 <bassilhamdi410@gmail.com>
2024-05-28 10:07:09 +0300

    alx push

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,16 +25,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	}
 	if (*separator == '\0')
-		flag = 0;
+		separator = "";
 	va_start(args, n);
 	for (i = 0; i < (int)n; i++)
 	{
 		value = va_arg(args, int);
 		if (i != (int)n - 1)
-			if (flag)
-				printf("%d%s", value, separator);
-			else
-				printf("%d", value);
+			printf("%d%s", value, separator);
 		else
 			printf("%d\n", value);
 	}
