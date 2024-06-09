0cc6a51e22cb59aa3e66ac8bf7f0cb213f65e15a
Basel-12 <bassilhamdi410@gmail.com>
2024-06-09 13:31:23 +0300

    alx push

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function
+ *
+ * @h: pointer to first node of list
+ *
+ * Description: function to print list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *cur = h;
+	size_t num = 0;
+
+	while (cur)
+	{
+		num++;
+		if ((cur->str) == '\0')
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", cur->len, cur->str);
+		cur = cur->next;
+	}
+	return (num);
+}
