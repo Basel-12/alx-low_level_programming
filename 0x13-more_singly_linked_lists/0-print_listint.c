52b3ebed8f2211c21236954f0a38b78b4c5c03ce
Basel-12 <bassilhamdi410@gmail.com>
2024-06-09 16:04:17 +0300

    alx push

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function
+ *
+ * @h: pointer to first node of list
+ *
+ * Description: function to print list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (num);
+}
