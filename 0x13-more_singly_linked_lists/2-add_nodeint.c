b53e2e28ed29a9179f7479d78bbe9472459c0541
Basel-12 <bassilhamdi410@gmail.com>
2024-06-09 16:14:24 +0300

    alx push

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -17,7 +17,7 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *new = (list_t *)malloc(sizeof(list_t));
+	listint_t *new = (listint_t *)malloc(sizeof(listint_t));
 
 	if (!*head || !new)
 		return (NULL);
