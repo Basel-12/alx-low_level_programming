328118eed78fcf3ec34773f6e1540eb9532dc746
Basel-12 <bassilhamdi410@gmail.com>
2024-06-10 11:23:25 +0300

    alx push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 	if (!*head)
 		*head = new;
+	if (idx == 0)
+        {
+            new->next = *head;
+            *head = new;
+        }
 	else
 	{
 		for (i = 0; i < idx - 1; i++)
