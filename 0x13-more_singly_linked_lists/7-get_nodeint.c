f4fd37960246c7e73928a1e8a75300f52e5c3c72
Basel-12 <bassilhamdi410@gmail.com>
2024-06-10 10:28:23 +0300

    alx push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,6 +8,8 @@
  *
  * @head: pointer to first node of list
  *
+ * @index: index we need
+ *
  * Description: function to add node to end of list.
  *
  * Return: number of nodes
@@ -15,7 +17,7 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *cur = head;
+	/*listint_t *cur = head;*/
 	int i;
 
 	if (!head)
