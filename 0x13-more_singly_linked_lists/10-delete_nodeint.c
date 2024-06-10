000e0ea5ffde38acba48c98368cec18cca3cde64
Basel-12 <bassilhamdi410@gmail.com>
2024-06-10 11:39:51 +0300

    alx push

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function
+ *
+ * @head: pointer to pointer to first node of list
+ *
+ *
+ * @index: index where node inserted
+ *
+ * Description: function to add node to end of list.
+ *
+ * Return: reference to new node
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *cur = *head, *del;
+	unsigned int i;
+
+	if (!head)
+		return (-1);
+	if (index == 0)
+	{
+		del = *head;
+		*head = del->next;
+		free(del);
+	}
+	else
+	{
+		for (i = 0; i < index - 1; i++)
+		{
+			if (!cur)
+				return (-1);
+			cur = cur->next;
+		}
+		del = cur->next;
+		cur->next = del->next;
+		free(del);
+	}
+	return (1);
+}
