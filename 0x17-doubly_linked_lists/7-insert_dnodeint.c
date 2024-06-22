f2a4820a3f851b55d134372543d8937e3f5d4ab1
Basel-12 <bassilhamdi410@gmail.com>
2024-06-22 21:44:35 +0300

    alx push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
 	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
-	dlistint_t *c = *h;
+	dlistint_t *cur = *h;
 
 	if (!h)
 		return (NULL);
