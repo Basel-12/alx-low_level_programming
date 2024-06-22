82a5ec1a5915b8ad20cd3c029ca276ef0fe096f2
Basel-12 <bassilhamdi410@gmail.com>
2024-06-22 21:51:47 +0300

    alx push

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		for (i = 0; i < idx && cur; i++)
 			cur = cur->next;
 		new->prev = cur->prev;
+		cur->prev->next = new;
 		cur->prev = new;
 		new->next = cur;
 	}
