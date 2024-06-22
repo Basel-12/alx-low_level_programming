e3ea7a01136f9c2c36fb4e603a6ff1b233dd9be6
Basel-12 <bassilhamdi410@gmail.com>
2024-06-22 20:58:46 +0300

    alx push

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - function free double linked list
+ *
+ * @head:pointer to list
+ *
+ * Return: void
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (*head)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
