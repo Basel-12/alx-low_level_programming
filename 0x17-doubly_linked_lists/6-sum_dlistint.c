58bb44f07bb6853e595b02c1d976839642eca7e4
Basel-12 <bassilhamdi410@gmail.com>
2024-06-22 21:32:26 +0300

    alx push

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - function return sum of double linked list
+ *
+ * @head: pinter to double linked list
+ *
+ * Return: sum of double linked list
+ *
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		h = h->next;
+	}
+	return (sum);
+}
