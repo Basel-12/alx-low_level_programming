05e91ad8c26ae59ee1ad6a6de8c8d0fd7fd6e338
Basel-12 <bassilhamdi410@gmail.com>
2024-06-22 21:10:34 +0300

    alx push

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
-	for (i = 0; i < index && head->next; i++)
+	for (i = 0; i < index && head; i++)
 		head = head->next;
 	return (head);
 }
